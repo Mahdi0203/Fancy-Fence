ecef39f0b1cee09b56cb069f398e2c6c78bf6a3d
Mah.D <125675444+Mahdi0203@users.noreply.github.com>
2023-07-27 20:15:39 +0600

    Create Fancy Fence.cpp

---
+++ Fancy Fence.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define Start ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define sort(a) sort(a.begin(),a.end())
+#define pb(b) push_back(b)
+#define reverse(a) reverse(a.begin(),a.end())
+#define tc ll t; cin>>t; while(t--)
+
+void solve()
+{
+    int a;
+    cin>>a;
+    if(360 % (180-a) == 0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    sort
+}
+int main()
+{
+    Start
+
+    tc
+    {
+        solve();
+    }
+}
+
+
